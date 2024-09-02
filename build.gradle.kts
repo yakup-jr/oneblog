a47f57dcc6012fb08b8558b74f3126b21262cddb
Dmitry <dimaakupovz@gmail.com>
2024-09-02 22:07:29 +0400

    style(build file): update indents
    change indents from space to tab

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,26 @@
 plugins {
-    java
-    id("org.springframework.boot") version "3.3.1"
-    id("io.spring.dependency-management") version "1.1.5"
+	java
+	id("org.springframework.boot") version "3.3.1"
+	id("io.spring.dependency-management") version "1.1.5"
 }
 
 group = "com.oneblog"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
 }
 
 configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
-    }
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
 }
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
@@ -37,5 +37,5 @@ dependencies {
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
+	useJUnitPlatform()
 }
