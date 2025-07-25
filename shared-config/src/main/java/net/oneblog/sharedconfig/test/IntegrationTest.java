8f106eda9cfd89b845076e7229b42fcce4418d68
Dmitry <dimaakupovz@gmail.com>
2025-07-25 23:06:24 +0400

    test(setup): add accidental config for test

---
+++ shared-config/src/main/java/net/oneblog/sharedconfig/test/IntegrationTest.java
@@ -0,0 +1,25 @@
+package net.oneblog.sharedconfig.test;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+//todo: fix migration bug with populate db on profile "test"
+
+/**
+ * The interface Integration test.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = {TestConfig.class})
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Sql(value = "/testdb/data.sql", executionPhase =
+    Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@ExtendWith(DatabaseCleanerExtension.class)
+public @interface IntegrationTest {
+}
